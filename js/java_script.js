7814da2b1ffefb12746fdf8c20a11435ce70936c
Mthabs <>
2023-04-11 09:17:28 +0000

    add continue button function to activate quiz and disabled instruction page

--- js/java_script.js
+++ js/java_script.js
@@ -13,3 +13,29 @@ const time_line = document.querySelector("header .time_line");
 const option_list = document.querySelector(".option_list");
 /*result*/ 
 const results_page = document.querySelector(".results_page");
+
+// if startQuiz button clicked
+start_quiz.onclick = ()=>{
+    instructions.classList.add("activeInfo"); //display instruction page
+}
+
+// if exitQuiz button clicked
+exit_btn.onclick = ()=>{
+    instructions.classList.remove("activeInfo"); //hide instruction page
+}
+// if continueQuiz button clicked
+continue_btn.onclick = ()=>{
+    instructions.classList.remove("activeInfo"); //hide instruction page
+    quiz_page.classList.add("activeQuiz"); //display quiz page
+    displayquestions(0); //calling showQestions function
+    questioncount(1); //passing 1 parameter to questioncount
+    startTimer(99); //calling startTimer function
+    startTimerLine(0); //calling startTimerLine function
+}
+let lengthvalue = 0;
+let allocated_time =  99;
+let participscore = 0;
+let question_index = 0;
+let questionNo = 1;
+let counter;
+let counterLine;
