69e10210e1ffaee3bd7d422d57e11c5df6e7b791
Mthabs <>
2023-04-11 09:06:44 +0000

    add all selection required elements

---
+++ js/java_script.js
@@ -0,0 +1,15 @@
+
+//selecting all required elements
+const start_quiz = document.querySelector(".home_page button");
+const instructions = document.querySelector(".instructions");
+/*buttons*/
+const exit_btn = instructions.querySelector(".buttons .exit_quiz");
+const continue_btn = instructions.querySelector(".buttons .restart");
+/*Quiz*/
+const quiz_page = document.querySelector(".quiz_page");
+const timeText = document.querySelector(".clock .show-time");
+const timeCount = document.querySelector(".clock .count_down");
+const time_line = document.querySelector("header .time_line");
+const option_list = document.querySelector(".option_list");
+/*result*/ 
+const results_page = document.querySelector(".results_page");
