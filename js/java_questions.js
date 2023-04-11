dadc1dd10f443b14bb2a5d76db90f2710b953385
Mthabs <>
2023-04-11 08:54:34 +0000

    add questions to java script file

---
+++ js/java_questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    number: 1,
+    question: "Which country won the first ever World Cup in 1930?",
+    answer: "Uruguay",
+    options: [
+      "England",
+      "USA",
+      "Uruguay",
+      "Argentina"
+    ]
+  },
+    {
+    number: 2,
+    question: "Which country has won the most World Cups?",
+    answer: "Brazil",
+    options: [
+      "France",
+      "Brazil",
+      "Argentina",
+      "Germany"
+    ]
+  },
+    {
+    number: 3,
+    question: "Which country has appeared in three World Cup finals, but never won the competition?",
+    answer: "Netherlands",
+    options: [
+      "Greece",
+      "Italy",
+      "Ukraine",
+      "Netherlands"
+    ]
+  },
+    {
+    number: 4,
+    question: "Which country hosted 2010 world cup?",
+    answer: "South Africa",
+    options: [
+      "Ghana",
+      "South Africa",
+      "Mexico",
+      "South Korea"
+    ]
+  },
+    {
+    number: 5,
+    question: "In which World Cup did Diego Maradona score his infamous 'Hand of God' goal?",
+    answer: "Mexico 1986",
+    options: [
+      "Mexico 1986",
+      "Italy 1968",
+      "Germany 1986",
+      "Argentina 1986"
+    ]
+  },
+  
+    {
+    number: 6,
+    question: "Which club has won the most Champions League titles?",
+    answer: "Real Madrid",
+    options: [
+      "FC Barcelona",
+      "Real Madrid",
+      "Chelsea",
+      "Liverpool"
+    ]
+  },
+    {
+    number: 7,
+    question: "Who is the Champions League's top goalscorer of all time?",
+    answer: "Cristiano Ronaldo",
+    options: [
+      "Lionel Messi",
+      "Cristiano Ronaldo",
+      "Diego Maradona",
+      "Pelé"
+    ]
+  },
+    {
+    number: 8,
+    question: "Which outfield player appeared in the Champions League final in three different decades?",
+    answer: "Ryan Giggs",
+    options: [
+      "Cristiano Ronaldo",
+      "Ryan Giggs",
+      "Ronaldinho Gaúcho",
+      "Lionel Messi"
+    ]
+  },
+    {
+    number: 9,
+    question: "Which club is sometimes referred to as FC Hollywood?",
+    answer: "Bayern Munich",
+    options: [
+      "Real Madrid",
+      "Manchester United",
+      "Tottenham",
+      "Bayern Munich"
+    ]
+  },
+    {
+    number: 10,
+    question: "In which year was the first European Championship held?",
+    answer: "1960",
+    options: [
+      "1960",
+      "1961",
+      "1962",
+      "1963"
+    ]
+  },
+];
